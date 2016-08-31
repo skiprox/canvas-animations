548c2fc62363daa56fb8ac07eeb933fd21bc8c05
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-08-31 12:48:11 -0400

    Adding in fake method tests for reference

--- test/spec/test.js
+++ test/spec/test.js
@@ -7,11 +7,22 @@ describe('CanvasAnimation', function () {
 	var canvasAnimation;
 
 	beforeEach(function(){
-
+		canvasAnimation = new CanvasAnimation(document.querySelector('.wrapper'));
 	});
 
 	it('should be a function', function () {
 		assert.equal('function', typeof CanvasAnimation);
 	});
+
+	// Method test
+	// describe('methodName', function () {
+	// 	it('should exist as a function', function () {
+	// 		assert.equal('function', typeof canvasAnimation.methodName);
+	// 	});
+	// 	it('should do what it is supposed to', function () {
+	// 		canvasAnimation.methodName();
+	// 		assert.equal(true, canvasAnimation.properties.someProperty);
+	// 	});
+	// });
 
 });
